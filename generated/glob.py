8e3b4656bb840d4af3111123c46cb44c7f1b0636
EsoCoding <arjanpoortman@gmail.com>
2023-03-11 02:24:18 +0100

    Compiling failed because of messed up directory structure becaue C folder got merged into main lib directory.

--- generated/glob.py
+++ generated/glob.py
@@ -13,12 +13,20 @@ def main(sourcePath):
     if path.exists(sourcePath):
         for root, dirs, files in os.walk(os.path.normpath(sourcePath)):
             for file in files:                
+                
                 if file.endswith(".d"):
-                    #foundFiles.append(os.path.join(root, file))
-                    foundFiles.append(os.path.relpath(os.path.join(os.path.relpath(sourcePath, start = sourcePath), root, file)))
-                    
+                    if file == 'functions.d' or file == "types.d":
+                        continue
+                    else:
+                        #foundFiles.append(os.path.join(root, file))
+                        #print (file)
+                        #foundFiles.append(file)
+                        foundFiles.append(os.path.relpath(os.path.join(os.path.relpath(sourcePath, start = sourcePath), root, file)))
+                    #foundFiles.append(os.path.relpath(os.path.join(os.path.relpath(sourcePath, start = sourcePath), root, file)))
+
     else:
         return FileNotFoundError("Path is not valid or does not exist!")
+
     return foundFiles
 
 if __name__ == '__main__':
@@ -26,4 +34,5 @@ if __name__ == '__main__':
     fileList = main(sys.argv[1:])
 
     for file in fileList:
+
         print(file)
